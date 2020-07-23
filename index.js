bdc64011a1574f6d59adc16cec4c129874b8f61b
Matt Chowning <matt.chowning@automattic.com>
2020-07-23 17:25:03 -0400

    Adding a file

--- index.js
+++ index.js
@@ -3,19 +3,29 @@ const github = require('@actions/github');
 const execSync = require('child_process').execSync;
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
 
-  const output = execSync('git status');
-  console.log(`output of: "${output}"`);
+    const filename = 'added_from_action';
+    const touchOutput = execSync(`touch ${filename}`);
+    console.log(`touchOutput: ${touchOutput}`);
 
-  const time = (new Date()).toTimeString();
-  core.setOutput("time: ", time);
+    const addOutput = execSync(`git add ${filename}`);
+    console.log(`addOutput: ${addOutput}`);
 
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+    const commitOutput = execSync('git commit -m "Adding file from within Action"');
+    console.log(`commitOutput: ${commitOutput}`);
+
+    const pushOutput = execSync('git push');
+    console.log(`pushOutput: ${pushOutput}`);
+
+    const time = (new Date()).toTimeString();
+    core.setOutput("time: ", time);
+
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
 } catch (error) {
-  core.setFailed(error.message);
+    core.setFailed(error.message);
 }
